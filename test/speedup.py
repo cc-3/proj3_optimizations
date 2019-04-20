23fddc03fd7b826b9db6e1b87a636cc1163eda31
Andres Castellanos <andres.cv@galileo.edu>
2019-04-19 20:32:50 -0600

    fix speedup script

--- test/speedup.py
+++ test/speedup.py
@@ -1,18 +1,25 @@
+import subprocess
 from subprocess import run
 from subprocess import PIPE
+
 
 def execute(cmd=[], shell=False, dir='.', input=None, encoding='ascii', timeout=5):
     return run(cmd, shell=shell, stdout=PIPE, stderr=PIPE, input=input, cwd=dir, timeout=timeout)
 
-def mean(script, runs=3):
+
+def speedup(script, runs=3):
     out = 0
     for i in range(runs):
-        task = execute(cmd=['./%s' % script, 'benchmark'], timeout=180)
+        task = execute(cmd=['./%s' % script, 'benchmark'], timeout=600)
         out += int(task.stdout.decode().strip().split('\n')[-1].split(' ')[0].strip())
     return out / runs
 
-execute(cmd=['make', 'clean'])
-execute(cmd=['make'])
-a = mean('optimized')
-b = mean('baseline')
-print('approx speedup: %dX' % (round(b / a)))
+
+try:
+    a = mean('optimized')
+    b = mean('baseline')
+    print('approx speedup: %dX' % (round(b / a)))
+except subprocess.TimeoutExpired:
+    print('TIMEOUT')
+except Exception:
+    print('UNEXPECTED EXCEPTION')
